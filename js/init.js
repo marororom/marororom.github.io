e5d89b660d48ed00f730b9f691e1e3e3dac2353a
Marcel <marcel@mmbp.local>
2015-04-19 22:12:24 +0200

    added navbar feature and anchor

--- js/init.js
+++ js/init.js
@@ -24,8 +24,27 @@
 		var	$window = $(window),
 			$body = $('body'),
 			$header = $('#header'),
-			$all = $body.add($header);
+			$all = $body.add($header),
+            sections = $('section'),
+            nav = $('nav'),
+            nav_height = nav.outerHeight() * 5;
 
+            $window.on('scroll', function () {
+                var cur_pos = $(this).scrollTop();
+
+                sections.each(function() {
+                    var top = $(this).offset().top - nav_height,
+                        bottom = top + $(this).outerHeight();
+
+                    if (cur_pos >= top && cur_pos <= bottom) {
+                        nav.find('a').removeClass('active');
+                        sections.removeClass('active');
+
+                        $(this).addClass('active');
+                        nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+                    }
+                });
+            });
 		// Disable animations/transitions until the page has loaded.
 			$body.addClass('is-loading');
 
